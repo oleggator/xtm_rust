f9031eb6e2d7dc8807ef7f13f3c3540feee4ef42
Oleg Utkin <o.utkin@corp.mail.ru>
2021-04-02 19:23:03 +0300

    move tx main loop to lib

--- examples/txapi/src/lib.rs
+++ examples/txapi/src/lib.rs
@@ -1,13 +1,8 @@
 use mlua::prelude::*;
-use xtm_rust::txapi::{tx_channel, Executor, Dispatcher, ExecError};
-use tokio::runtime;
+use xtm_rust::txapi::{Dispatcher, run_module};
 use tokio::time::Instant;
 
-
-type R = i32;
-type F = Box<dyn FnOnce() -> R + Send + 'static>;
-
-async fn module_main(dispatcher: Dispatcher<F, R>) {
+async fn module_main(dispatcher: Dispatcher<Box<dyn FnOnce() -> i32 + Send + 'static>, i32>) {
     let iterations = 1_000_000;
 
     let now = Instant::now();
@@ -23,45 +18,12 @@ async fn module_main(dispatcher: Dispatcher<F, R>) {
     println!("iterations: {} | elapsed: {}ns | avg per cycle: {}ns", iterations, elapsed, avg);
 }
 
-fn tx_main(executor: Box<Executor<F, R>>) -> i32 {
-    loop {
-        match executor.exec() {
-            Ok(_) => {}
-            Err(ExecError::RXChannelClosed) => {
-                break 0;
-            }
-            Err(err) => {
-                println!("{:#?}", err);
-                break 1;
-            }
-        }
-    }
-}
-
 #[mlua::lua_module]
 fn libtxapi(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
 
     exports.set("start", lua.create_function_mut(|_: &Lua, (buffer, ): (usize, )| {
-        let (dispatcher, executor) = tx_channel(buffer);
-
-        let module_thread = std::thread::Builder::new()
-            .name("module".to_string())
-            .spawn(move || {
-                runtime::Builder::new_current_thread()
-                    .enable_io()
-                    .build()
-                    .unwrap()
-                    .block_on(module_main(dispatcher))
-            })
-            .unwrap();
-
-        let mut tx_fiber = tarantool::fiber::Fiber::new("module_tx", &mut tx_main);
-        tx_fiber.set_joinable(true);
-        tx_fiber.start(executor);
-
-        tx_fiber.join();
-        module_thread.join().unwrap();
+        run_module(buffer, module_main);
         Ok(())
     })?)?;
 
--- src/txapi.rs
+++ src/txapi.rs
@@ -2,6 +2,8 @@ use tokio::sync::oneshot;
 use async_channel;
 use async_channel::TryRecvError;
 use tarantool::fiber;
+use tokio::runtime;
+use std::future::Future;
 
 type Task<F, R> = (F, oneshot::Sender<R>);
 type TaskTX<F, R> = async_channel::Sender<Task<F, R>>;
@@ -64,9 +66,57 @@ impl<F, R> Executor<F, R> where F: FnOnce() -> R + Send + 'static {
     }
 }
 
+pub fn run_module<F, Fut, R, M>(buffer: usize, module_main: M)
+    where
+        R: Send + 'static,
+        F: FnOnce() -> R + Send + 'static,
+        M: FnOnce(Dispatcher<F, R>) -> Fut + Send + 'static,
+        Fut: Future<Output=()>,
+{
+    let (dispatcher, executor): (
+        Dispatcher<F, R>,
+        Executor<F, R>,
+    ) = tx_channel(buffer);
+
+    let module_thread = std::thread::Builder::new()
+        .name("module".to_string())
+        .spawn(move || {
+            runtime::Builder::new_current_thread()
+                .enable_io()
+                .build()
+                .unwrap()
+                .block_on(module_main(dispatcher))
+        })
+        .unwrap();
+
+    let mut tx_fiber = tarantool::fiber::Fiber::new("module_tx", &mut tx_main);
+    tx_fiber.set_joinable(true);
+    tx_fiber.start(executor);
+
+    tx_fiber.join();
+    module_thread.join().unwrap();
+}
+
 pub fn tx_channel<F, R>(buffer: usize) -> (Dispatcher<F, R>, Executor<F, R>)
     where F: FnOnce() -> R + Send + 'static
 {
     let (task_tx, task_rx) = async_channel::bounded(buffer);
     (Dispatcher::new(task_tx), Executor::new(task_rx))
+}
+
+pub fn tx_main<F, R>(executor: Box<Executor<F, R>>) -> i32
+    where F: FnOnce() -> R + Send + 'static
+{
+    loop {
+        match executor.exec() {
+            Ok(_) => {}
+            Err(ExecError::RXChannelClosed) => {
+                break 0;
+            }
+            Err(err) => {
+                println!("{:#?}", err);
+                break 1;
+            }
+        }
+    }
 }
