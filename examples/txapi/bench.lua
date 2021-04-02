63bd5f845fc55587b6fad1a456cb8e7aa50b0c26
Oleg Utkin <o.utkin@corp.mail.ru>
2021-04-02 19:57:22 +0300

    add lua result calculation

--- examples/txapi/bench.lua
+++ examples/txapi/bench.lua
@@ -1,6 +1,30 @@
+box.cfg {
+    wal_mode = 'none',
+}
+local space = box.schema.space.create('some_space', {
+    if_not_exists = true,
+})
+space:create_index('pk', {
+    parts = { { 1, 'unsigned' } },
+    if_not_exists = true,
+})
+
 print('Rust module:')
 local txapi = require('libtxapi')
 txapi.start(16)
 
+print('Lua:')
+local clock = require('clock')
+local tuple = { 1, 'some_string' }
+local iteration = 1000000
 
-os.exit()
+-- TODO fix result calculation
+local begin = clock.time64() * 1000
+for _ = 1, iteration do
+    space:replace(tuple)
+end
+local elapsed = clock.time64() * 1000 - begin
+local per_cycle = elapsed / iteration
+print('iteration:', iteration, 'elapsed:', elapsed, 'avg per cycle:', per_cycle)
+
+require('console'):start()
