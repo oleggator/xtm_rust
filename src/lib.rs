c1a31190169261031c7280ae1ee5d204a8a2aee9
Oleg Utkin <oleg_utkin@me.com>
2021-06-01 08:45:58 +0000

    remove type parameter for run_module

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,12 @@ use crossbeam_utils::thread;
 mod eventfd;
 pub mod txapi;
 
-pub fn run_module<Fut, M, T>(buffer: usize, module_main: M) -> io::Result<T>
+pub fn run_module<Fut, M>(buffer: usize, module_main: M) -> io::Result<Fut::Output>
 where
     M: FnOnce(AsyncDispatcher) -> Fut,
     M: Send,
-    Fut: Future<Output = T>,
-    T: Send,
+    Fut: Future,
+    Fut::Output: Send,
 {
     let (dispatcher, executor) = channel(buffer)?;
 
@@ -21,7 +21,7 @@ where
         let module_thread = scope
             .builder()
             .name("module".to_string())
-            .spawn(move |_| -> io::Result<T> {
+            .spawn(move |_| -> io::Result<Fut::Output> {
                 let rt = runtime::Builder::new_multi_thread().enable_io().build()?;
                 rt.block_on(async move {
                     let async_dispather = AsyncDispatcher::try_from(dispatcher)?;
