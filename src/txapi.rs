02ec709b05b9f1036e70dead1335e46485593ffc
Oleg Utkin <oleg_utkin@me.com>
2021-09-28 19:01:37 +0300

    user flume instead of async_channel

--- src/txapi.rs
+++ src/txapi.rs
@@ -1,15 +1,15 @@
 use tokio::sync::oneshot;
-use async_channel;
-use async_channel::TryRecvError;
 use thiserror::Error;
 use crate::eventfd;
 use std::{convert::TryFrom, os::unix::io::{AsRawFd, RawFd}};
 use std::io;
 use mlua::Lua;
 
+use flume::{self, TryRecvError};
+
 type Task = Box<dyn FnOnce(&Lua) -> Result<(), ChannelError> + Send>;
-type TaskSender = async_channel::Sender<Task>;
-type TaskReceiver = async_channel::Receiver<Task>;
+type TaskSender = flume::Sender<Task>;
+type TaskReceiver = flume::Receiver<Task>;
 
 #[derive(Error, Debug)]
 pub enum ChannelError {
@@ -59,7 +59,7 @@ impl AsyncDispatcher {
             result_tx.send(result).or(Err(ChannelError::TXChannelClosed))
         });
 
-        if let Err(_channel_closed) = self.task_tx.send(handler_func).await {
+        if let Err(_channel_closed) = self.task_tx.send_async(handler_func).await {
             return Err(ChannelError::TXChannelClosed);
         }
 
@@ -106,7 +106,7 @@ impl Executor {
                 match self.task_rx.try_recv() {
                     Ok(func) => return func(lua),
                     Err(TryRecvError::Empty) => tarantool::fiber::sleep(0.),
-                    Err(TryRecvError::Closed) => return Err(ChannelError::RXChannelClosed),
+                    Err(TryRecvError::Disconnected) => return Err(ChannelError::RXChannelClosed),
                 };
             }
             let _ = self.eventfd.coio_read(1.0);
@@ -121,7 +121,7 @@ impl AsRawFd for Executor {
 }
 
 pub fn channel(buffer: usize) -> io::Result<(Dispatcher, Executor)> {
-    let (task_tx, task_rx) = async_channel::bounded(buffer);
+    let (task_tx, task_rx) = flume::bounded(buffer);
     let efd = eventfd::EventFd::new(0, false)?;
 
     Ok((Dispatcher::new(task_tx, efd.try_clone()?), Executor::new(task_rx, efd)))
